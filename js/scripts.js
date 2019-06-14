d585d9e8ac9a6dd00737312f9636dc80cd2b2596
Vladimir Samojlov <v-moi@ya.ru>
2019-06-14 11:29:03 +0300

    edit header

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+if (!$('body').hasClass('page-template-tpl_main')) {
+  $('header').css({'background-color': '#692936'});
+}
+
 $('.toogle-menu').on('click', function(e) {
   e.preventDefault;
   $(this).toggleClass('toogle-menu_active');
@@ -7,10 +11,16 @@ $('.toogle-menu').on('click', function(e) {
 
 $(window).scroll(function(){
   var h_scroll = $(this).scrollTop();
-  if (h_scroll > 56) {
-    $('header').addClass('header__fixed')
+  if (h_scroll > 90) {
+    $('header').addClass('header__fixed');
+    $('header').css({'background-color': '#692936'});
   } else {
-    $('header').removeClass('header__fixed')
+    if (!$('body').hasClass('page-template-tpl_main')) {
+      $('header').removeClass('header__fixed');
+    } else {
+      $('header').css({'background-color': 'transparent'});
+    }
+    
   }
 })
 
